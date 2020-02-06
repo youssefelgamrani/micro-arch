1eefdd004db0281b29ba56e2ac6f9f496740734a
gamraniyoussef <gamraniyoussef@gmail.com>
2020-02-06 15:40:32 +0100

    fix style

--- support/gateway-server/src/main/java/com/gateway/GatewayServerApplication.java
+++ support/gateway-server/src/main/java/com/gateway/GatewayServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GatewayServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GatewayServerApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayServerApplication.class, args);
+  }
 }
